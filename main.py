9626a68af72576154ef5e8bf29d259f28d62dcd0
AlphaqMehmet <98256509+AlphaqMehmet@users.noreply.github.com>
2024-02-27 23:04:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 from selenium import webdriver
 
-"""
-service = webdriver.ChromeService(executable_path = 'chromedriver.exe')
-options = webdriver.ChromeOptions() 
-#options.add_experimental_option("excludeSwitches", ["enable-logging"])
-#options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(service=service)
-"""
 driver = webdriver.Chrome()
 driver.get("https://www.google.com")
 
